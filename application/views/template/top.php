c94ec6a3d0cc52017789dc7df436b0e180d12ae0
Schuyler Cumbie <schuylerc@gmail.com>
2014-12-25 02:42:53 -0500

    add initial theme

---
+++ application/views/template/top.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta charset="utf-8">
+		<title>BlueSangria</title>
+		<meta name="generator" content="Bootply" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+		<!--[if lt IE 9]>
+			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<link href="/assets/css/styles.css" rel="stylesheet">
+	</head>
+	<body>
+<div class="wrapper">
+    <div class="box">
+        <div class="row row-offcanvas row-offcanvas-left">
+                      
+          
+            <!-- sidebar -->
+            <div class="column col-sm-2 col-xs-1 sidebar-offcanvas" id="sidebar">
+              
+              	<ul class="nav">
+          			<li><a href="#" data-toggle="offcanvas" class="visible-xs text-center"><i class="glyphicon glyphicon-chevron-right"></i></a></li>
+            	</ul>
+               
+                <ul class="nav hidden-xs" id="lg-menu">
+                    <li class="active"><a href="#featured"><i class="glyphicon glyphicon-dashboard"></i> Dashboard</a></li>
+                    <li><a href="#stories"><i class="glyphicon glyphicon-camera"></i> Cameras</a></li>
+                    <li><a href="#"><i class="glyphicon glyphicon-off"></i> Lights</a></li>
+                    <li><a href="#"><i class="glyphicon glyphicon-transfer"></i> Scenes</a></li>
+                    <li><a href="#"><i class="glyphicon glyphicon-floppy-disk"></i> Programs</a></li>
+                    <li><a href="#"><i class="glyphicon glyphicon-globe"></i> Climate</a></li>
+                    <li><a href="#"><i class="glyphicon glyphicon-hdd"></i> Network</a></li>
+
+                </ul>
+                <ul class="list-unstyled hidden-xs" id="sidebar-footer">
+                    <li>
+                      <a href="http://www.bootply.com"><h3>Bootstrap</h3> <i class="glyphicon glyphicon-heart-empty"></i> Bootply</a>
+                    </li>
+                </ul>
+              
+              	<!-- tiny only nav-->
+              <ul class="nav visible-xs" id="xs-menu">
+                  	<li class="active"><a href="#featured"><i class="glyphicon glyphicon-dashboard"></i></a></li>
+                    <li><a href="#stories"><i class="glyphicon glyphicon-camera"></i></a></li>
+                    <li><a href="#"><i class="glyphicon glyphicon-off"></i></a></li>
+                    <li><a href="#"><i class="glyphicon glyphicon-transfer"></i></a></li>
+                    <li><a href="#"><i class="glyphicon glyphicon-floppy-disk"></i></a></li>
+                    <li><a href="#"><i class="glyphicon glyphicon-globe"></i></a></li>
+                    <li><a href="#"><i class="glyphicon glyphicon-hdd"></i></a></li>
+                </ul>
+              
+            </div>
+            <!-- /sidebar -->
+          
+            <!-- main right col -->
+            <div class="column col-sm-10 col-xs-11" id="main">
+                
+                <!-- top nav -->
+              	<div class="navbar navbar-blue navbar-static-top">  
+                    <div class="navbar-header">
+                      <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle</span>
+                        <span class="icon-bar"></span>
+          				<span class="icon-bar"></span>
+          				<span class="icon-bar"></span>
+                      </button>
+                      <a href="/" class="navbar-brand logo">B</a>
+                  	</div>
+                  	<nav class="collapse navbar-collapse" role="navigation">
+                    <form class="navbar-form navbar-left">
+                        <div class="input-group input-group-sm" style="max-width:360px;">
+                          <input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
+                          <div class="input-group-btn">
+                            <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                          </div>
+                        </div>
+                    </form>
+                    <ul class="nav navbar-nav">
+                      <li>
+                        <a href="/"><i class="glyphicon glyphicon-home"></i> Home</a>
+                      </li>
+                      <!--
+                      <li>
+                        <a href="#postModal" role="button" data-toggle="modal"><i class="glyphicon glyphicon-plus"></i> Post</a>
+                      </li>
+                      <li>
+                        <a href="#"><span class="badge">badge</span></a>
+                      </li>
+                      -->
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                      <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-user"></i></a>
+                        <ul class="dropdown-menu">
+                          <li><a href="">On Network (sign in not required)</a></li>
+                        </ul>
+                      </li>
+                    </ul>
+                  	</nav>
+                </div>
+                <!-- /top nav -->
+
+
+                <div class="padding">
+                    <div class="full col-sm-9">
+
